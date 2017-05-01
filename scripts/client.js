6cbb5687fd3123ecc91efa66afcff3f786a19194
Chris Stanton <cstanton0760@yahoo.com>
2017-05-01 12:26:19 -0500

    first commit

---
+++ scripts/client.js
@@ -0,0 +1,14 @@
+
+var app = angular.module('AngularApp', []);
+
+app.controller('MainController', function () {
+
+    var self = this;
+    self.message = "Angular is sourced correctly"
+
+
+
+
+
+
+});//ennd of app.controller
